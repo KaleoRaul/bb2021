2b9b0c9a44664db0de49ca7645ca566876bdf422
KaleoRaul <87835806+KaleoRaul@users.noreply.github.com>
2021-07-22 22:01:46 -0300

    teste
    new

--- .next/static/webpack/webpack.d51f5d43d6ba2eba12c8.hot-update.js
+++ .next/static/webpack/webpack.d51f5d43d6ba2eba12c8.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9f8c7dbd8c739a12acde"; }
+/******/ 	__webpack_require__.h = function() { return "df782a47da3a85249b57"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
